1324fcd1d97bb572e3ae3a3b4606db8885cef3f0
Hariharan <srinathh@gmail.com>
2021-08-07 23:32:57 +0800

    Press q to quit

--- main.go
+++ main.go
@@ -12,13 +12,17 @@ func main() {
 	r := bufio.NewReader(os.Stdin)
 
 	for {
-		_, err := r.ReadByte()
+		b, err := r.ReadByte()
 
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			fmt.Fprintf(os.Stderr, "Error: reading key from Stdin: %s\n", err)
 			os.Exit(1)
+		}
+
+		if b == 'q' {
+			break
 		}
 	}
 }
