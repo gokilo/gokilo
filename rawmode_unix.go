e98dd385570c839edadc9c6e7d78dc5de9876e75
Hariharan <srinathh@gmail.com>
2021-08-08 18:15:19 +0800

    Turn off canonical mode

--- rawmode_unix.go
+++ rawmode_unix.go
@@ -20,7 +20,7 @@ func rawMode() (func(), error) {
 
 	copy := *termios
 
-	termios.Lflag = termios.Lflag &^ unix.ECHO
+	termios.Lflag = termios.Lflag &^ (unix.ECHO | unix.ICANON)
 
 	if err := unix.IoctlSetTermios(unix.Stdin, unix.TCSETSF, termios); err != nil {
 		return nil, fmt.Errorf("rawMode: error setting terminal flags: %w", err)
